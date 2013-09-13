2ece31bc3e5871dc380ba5553e2c4b61fd081139
guilherme.p.pereira@gmail.com <guilherme.p.pereira@gmail.com@0e6e3891-25a7-1c5c-9f8a-555bef0c9934>
2013-09-13 03:01:40 +0000

    Acrescentados layouts das novas telas de avaliação de acordo com os protótipos.

--- trunk/Desenvolvimento/avadoc-022013/avadoc-web/src/main/java/br/ufg/inf/avadoc/managedbean/AvaliacaoManagedBean.java
+++ trunk/Desenvolvimento/avadoc-022013/avadoc-web/src/main/java/br/ufg/inf/avadoc/managedbean/AvaliacaoManagedBean.java
@@ -1,18 +1,45 @@
 package br.ufg.inf.avadoc.managedbean;
 
+import br.ufg.inf.avadoc.entity.Docente;
+import br.ufg.inf.avadoc.model.enums.EnumTipoAvaliacao;
+import java.io.Serializable;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Scope("session")
 @Component(AvaliacaoManagedBean.KEY_COMPONENT)
-public class AvaliacaoManagedBean extends AbstractManagedBean {
+public class AvaliacaoManagedBean extends AbstractManagedBean implements Serializable {
 
-	public static final String KEY_COMPONENT = "avaliacaoManagedBean";
+    public static final String KEY_COMPONENT = "avaliacaoManagedBean";
+    private static final long serialVersionUID = -7047823076987573930L;
+    private Docente docente;
+    private String matriculaSelecionada;
+    private String tipoAvaliacao;
 
-	private static final long serialVersionUID = -7047823076987573930L;
+    public String getMatriculaSelecionada() {
+        return matriculaSelecionada;
+    }
 
-	public void metodo() {
+    public void setMatriculaSelecionada(String matriculaSelecionada) {
+        this.matriculaSelecionada = matriculaSelecionada;
+    }
+    
+    public Docente getDocente() {
+        return docente;
+    }
 
-	}
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
 
+    public void setTipoAvaliacao(String tipoAvaliacao) {
+        this.tipoAvaliacao = tipoAvaliacao;
+    }
+
+    public String getTipoAvaliacao() {
+        return tipoAvaliacao;
+    }
+    
+    public void metodo() {
+    }
 }
