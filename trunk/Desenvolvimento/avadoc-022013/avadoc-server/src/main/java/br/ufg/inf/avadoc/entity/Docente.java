96c089b5c4c71ea1f9b5f954a77032faa14ffabc
daynilou@gmail.com <daynilou@gmail.com@0e6e3891-25a7-1c5c-9f8a-555bef0c9934>
2013-09-15 22:02:28 +0000

    Implementação de atributos necessários no docente e que até então não haviam sido criados.

--- trunk/Desenvolvimento/avadoc-022013/avadoc-server/src/main/java/br/ufg/inf/avadoc/entity/Docente.java
+++ trunk/Desenvolvimento/avadoc-022013/avadoc-server/src/main/java/br/ufg/inf/avadoc/entity/Docente.java
@@ -1,7 +1,7 @@
 package br.ufg.inf.avadoc.entity;
 
 import java.io.Serializable;
-
+import java.util.Calendar;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -31,11 +31,37 @@ public class Docente extends AbstractEntity implements Serializable {
 	 */
 	@Column(unique=true)
 	private String matricula;
+	
 	/**
 	 * Nome do docente avaliado
 	 */
 	private String nome;
-
+	
+	/**
+	 * Matrícula SIAP do docente.
+	 */
+	private String matriculaSIAP;
+	
+	/**
+	 * Lotação do docente.
+	 */
+	private String lotacao;
+	
+	/**
+	 * Data de ingresso do professor.
+	 */
+	private Calendar dataIngresso;
+	
+	/**
+	 * Classe do professor.
+	 */
+	private String classe;
+	
+	/**
+	 * Nível do professor.
+	 */
+	private String nivel;
+	
 	/**
 	 * Retorna matrícula do docente
 	 * 
@@ -87,4 +113,83 @@ public class Docente extends AbstractEntity implements Serializable {
 		this.nome = nome;
 	}
 
+	/**
+	 * Obtém a lotação do docente.
+	 * @return Lotação do docente.
+	 */
+	public String getLotacao() {
+		return lotacao;
+	}
+
+	/**
+	 * Define a lotação do docente.
+	 * @param lotacao Descrição/Código da lotação.
+	 */
+	public void setLotacao(String lotacao) {
+		this.lotacao = lotacao;
+	}
+
+	/**
+	 * Obtém a data de ingresso do docente.
+	 * @return Data de ingresso do docente.
+	 */
+	public Calendar getDataIngresso() {
+		return dataIngresso;
+	}
+
+	/**
+	 * Define a data de ingresso do docente.
+	 * @param dataIngresso Data de ingresso do docente.
+	 */
+	public void setDataIngresso(Calendar dataIngresso) {
+		this.dataIngresso = dataIngresso;
+	}
+
+	/**
+	 * Obtém a classe do docente.
+	 * @return Classe do docente.
+	 */
+	public String getClasse() {
+		return classe;
+	}
+
+	/**
+	 * Define a classe do docente.
+	 * @param classe Classe do docente.
+	 */
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+
+	/**
+	 * Obtém o nível do docente.
+	 * @return Nível do docente.
+	 */
+	public String getNivel() {
+		return nivel;
+	}
+
+	/**
+	 * Define o nível do docente.
+	 * @param nivel Nível do docente.
+	 */
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+	/**
+	 * Obtém a matrícula SIAP do docente.
+	 * @return Matrícula SIAP do docente.
+	 */
+	public String getMatriculaSIAP() {
+		return matriculaSIAP;
+	}
+
+	/**
+	 * Define a matrícula SIAP do docente.
+	 * @param matriculaSIAP Matrícula SIAP do docente.
+	 */
+	public void setMatriculaSIAP(String matriculaSIAP) {
+		this.matriculaSIAP = matriculaSIAP;
+	}
 }
