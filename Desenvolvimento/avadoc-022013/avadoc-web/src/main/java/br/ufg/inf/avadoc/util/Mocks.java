16c07e1654475f79a9cc1ae79e98dbe19b67ffdd
daynilou <daynilou@gmail.com>
2013-09-15 22:05:08 +0000

    Inclusão do botão buscar. Implementação de mock para trazer os dados do docente. Implementação do managedbean/AvaliacaoManagedBean.java para buscar os dados do docente e exibí-los na tela.

---
+++ Desenvolvimento/avadoc-022013/avadoc-web/src/main/java/br/ufg/inf/avadoc/util/Mocks.java
@@ -0,0 +1,34 @@
+package br.ufg.inf.avadoc.util;
+
+import java.util.Calendar;
+import br.ufg.inf.avadoc.entity.Docente;
+
+public class Mocks {
+	
+	public static Docente obtenhaDocentePorMatricula(String matricula) {
+		Docente docente = new Docente();
+		Calendar cal = Calendar.getInstance();
+		
+		if (matricula.equals("1")) {
+			docente.setNome("Fulano de Tal");
+			cal.set(2009, Calendar.DECEMBER, 12);
+			docente.setClasse("B");
+			docente.setDataIngresso(cal);			
+			docente.setLotacao("Faculdade de Letras - UFG");
+			docente.setMatriculaSIAP("123" + matricula);			
+			docente.setNivel("I");
+		} else if (matricula.equals("2")){
+			docente.setNome("Joaquim das Quintas");
+			cal.set(2010, Calendar.JANUARY, 1);
+			docente.setClasse("A");
+			docente.setDataIngresso(cal);
+			docente.setLotacao("Instituto de Informática - UFG");
+			docente.setMatriculaSIAP("321" + matricula);
+			docente.setNivel("V");
+		} else {
+			return null;
+		}		
+		
+		return docente;
+	}
+}
