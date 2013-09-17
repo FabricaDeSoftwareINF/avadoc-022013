5411cd9dc354e39920f0d19020bdebaa92d942b9
ciroanacleto@gmail.com <ciroanacleto@gmail.com@0e6e3891-25a7-1c5c-9f8a-555bef0c9934>
2013-09-17 00:16:54 +0000

    Criada a classe AbstractAtividade que contem o atributo 'pontos' e os metodos get e set pontos que servirão de base para para todas as atividades e producoes. Esta classe deve substituir a classe de interface IAtividade.

---
+++ trunk/Desenvolvimento/avadoc-022013/avadoc-server/src/main/java/br/ufg/inf/avadoc/entity/AbstractAtividade.java
@@ -0,0 +1,21 @@
+package br.ufg.inf.avadoc.entity;
+
+/**
+ * Classe abstrata que mant&eacute;m o atributo de pontos utilizado em todas as
+ * atividades.
+ * */
+public abstract class AbstractAtividade extends AbstractEntity{
+	
+	protected double pontos;
+
+	public double getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(double pontos) {
+		this.pontos = pontos;
+	}
+	
+	
+
+}
